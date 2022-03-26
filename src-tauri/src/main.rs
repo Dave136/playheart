ea041619d288484f1a91de105c79e69fa6602285
Dave136 <davejs136@gmail.com>
2022-03-26 19:12:52 -0400

    add: generate-handler fn

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,13 @@
   windows_subsystem = "windows"
 )]
 
+mod commands;
+
+use commands::get_songs;
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_songs])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
