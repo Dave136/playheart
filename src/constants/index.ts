878c62c9939848938c692c7fefe8b5bf85eecfa6
Dave136 <davejs136@gmail.com>
2022-04-05 18:23:15 -0400

    add: constants directory

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const enum Colors {
+  Black = '#000',
+  White = '#fff',
+  Primary = '#ff482f',
+  Secondary = '#ebebeb',
+  SecondaryAlt = '#e5e5e5',
+  Font = '#3e3e3e',
+  Highlight = '#c4c4c4',
+}
